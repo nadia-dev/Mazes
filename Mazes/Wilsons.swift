78abaa6ec48622b6d1dfcc204b06dc4f87022234
Nadia <joystate@me.com>
2018-04-23 17:11:55 -0400

    remove incorrect wilsons implementation

--- Mazes/Wilsons.swift
+++ Mazes/Wilsons.swift
@@ -11,36 +11,7 @@ import Foundation
 class Wilsons {
 
     class func on(gridConstructor: GridConstructor) -> GridConstructor {
-        var unvisited: [Cell] = []
-        gridConstructor.forEachCell { (cell) in
-            unvisited.append(cell)
-        }
-        let first = unvisited.randomItem()
-        print("first: \(first.description())")
-        unvisited.delete(cell: first)
-        while unvisited.count != 0 {
-            var cell = unvisited.randomItem()
-            print("random cell: \(cell.description())")
-            var path = [cell]
-            print("path set to random cell: \(path.description())")
-            print("unvisited cells: \(unvisited.description())")
-            while unvisited.contains(cell) {
-                cell = cell.neighborCells.randomItem()
-                print("random neighbor: \(cell.description())")
-                if let position = path.index(of: cell) {
-                    print("position of random neighbor: \(position)")
-                    path = Array(path.prefix(upTo: position+1))
-                    print("updated path: \(path.description())")
-                } else {
-                    path.append(cell)
-                    print("path after neighbor append: \(path.description())")
-                }
-            }
-            for i in 0..<path.count - 2 {
-                path[i].link(cell: path[i+1])
-                unvisited.delete(cell: path[i])
-            }
-        }
+        // TODO: - to implement
         return gridConstructor
     }
     

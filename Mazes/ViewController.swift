77824ff2be851fc08cf221a98ee8d3ddbb3d5c8c
Nadia Yudina <joystate@me.com>
2018-10-14 15:24:57 -0700

    add framework file

--- Mazes/ViewController.swift
+++ Mazes/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MazeBuilder
+
 
 class ViewController: UIViewController {
     
@@ -14,55 +16,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var mazeContainerHeight: NSLayoutConstraint?
     @IBOutlet weak var mazeContainerWidth: NSLayoutConstraint?
 
-    private var cellSide: CGFloat = 0.0
-    private let offset: CGFloat = 20
-    private let algorithm = LongestPath(withRows: Constants.rows, columns: Constants.columns, creationAlgorithmType: .wilsons, startCellPositionType: .center)
+//    private var cellSide: CGFloat = 0.0
+//    private let offset: CGFloat = 20
+//    private let algorithm = LongestPath(withRows: Constants.rows, columns: Constants.columns, creationAlgorithmType: .wilsons, startCellPositionType: .center)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureMatrix()
-        runAlgorithm()
+//        configureMatrix()
+//        runAlgorithm()
+        Hello.greeting()
     }
     
-    private func runAlgorithm() {
-        let presentation = Presentation(withGridConstructor: algorithm.constructor, start: algorithm.startCell)
-        guard let distances = presentation.showAllDistances() else {
-            return
-        }
-        print("All distances")
-        print(distances)
-        
-        let originalDistances = algorithm.constructor?.distances
-        if let goalCell = algorithm.constructor?.currentGrid()?[Constants.rows-1][0] {
-            print("Searching for path to goal")
-            algorithm.constructor?.distances?.pathToGoal(goal: goalCell, completion: { (distancesToGoal) in
-                algorithm.constructor?.distances = distancesToGoal
-                if let description = algorithm.constructor?.description() {
-                    print("Path from northwest corner to southwest corner:")
-                    print(description)
-                }
-            })
-        }
-        
-        showCells(forDistances: originalDistances)
-    }
-    
-    private func configureMatrix() {
-        let viewHeight = self.view.frame.height - offset
-        let viewWidth = self.view.frame.width - offset
-        cellSide = min(viewWidth/CGFloat(Constants.columns), viewHeight/CGFloat(Constants.rows))
-        mazeContainerWidth?.constant = cellSide * CGFloat(Constants.columns)
-        mazeContainerHeight?.constant = cellSide * CGFloat(Constants.rows)
-    }
-    
-    private func showCells(forDistances distances: Distances?) {
-        algorithm.constructor?.forEachCell { (cell) in
-            let x = CGFloat(cell.column) * cellSide
-            let y = CGFloat(cell.row) * cellSide
-            let cellView = CellView.create(cell: cell, cellSide: cellSide, x: x, y: y, distances: distances)
-            mazeContainer?.addSubview(cellView)
-        }
-    }
+//    private func runAlgorithm() {
+//        let presentation = Presentation(withGridConstructor: algorithm.constructor, start: algorithm.startCell)
+//        guard let distances = presentation.showAllDistances() else {
+//            return
+//        }
+//        print("All distances")
+//        print(distances)
+//        
+//        let originalDistances = algorithm.constructor?.distances
+//        if let goalCell = algorithm.constructor?.currentGrid()?[Constants.rows-1][0] {
+//            print("Searching for path to goal")
+//            algorithm.constructor?.distances?.pathToGoal(goal: goalCell, completion: { (distancesToGoal) in
+//                algorithm.constructor?.distances = distancesToGoal
+//                if let description = algorithm.constructor?.description() {
+//                    print("Path from northwest corner to southwest corner:")
+//                    print(description)
+//                }
+//            })
+//        }
+//        
+//        showCells(forDistances: originalDistances)
+//    }
+//    
+//    private func configureMatrix() {
+//        let viewHeight = self.view.frame.height - offset
+//        let viewWidth = self.view.frame.width - offset
+//        cellSide = min(viewWidth/CGFloat(Constants.columns), viewHeight/CGFloat(Constants.rows))
+//        mazeContainerWidth?.constant = cellSide * CGFloat(Constants.columns)
+//        mazeContainerHeight?.constant = cellSide * CGFloat(Constants.rows)
+//    }
+//    
+//    private func showCells(forDistances distances: Distances?) {
+//        algorithm.constructor?.forEachCell { (cell) in
+//            let x = CGFloat(cell.column) * cellSide
+//            let y = CGFloat(cell.row) * cellSide
+//            let cellView = CellView.create(cell: cell, cellSide: cellSide, x: x, y: y, distances: distances)
+//            mazeContainer?.addSubview(cellView)
+//        }
+//    }
 
 }
 
